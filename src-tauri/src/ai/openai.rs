a4c7be7773a7d35291c3c833ec074e3ec7ac591d
DreamingCodes <me@dreaming.codes>
2023-12-01 23:49:22 +0100

    Fixed image_to_base64url function

--- src-tauri/src/ai/openai.rs
+++ src-tauri/src/ai/openai.rs
@@ -1,21 +1,19 @@
+use std::io::{Cursor};
 use async_openai::config::OpenAIConfig;
-use async_openai::types::{ChatCompletionRequestMessage, ChatCompletionRequestMessageContentPart, ChatCompletionRequestMessageContentPartImage, ChatCompletionRequestMessageContentPartImageArgs, ChatCompletionRequestMessageContentPartText, ChatCompletionRequestMessageContentPartTextArgs, ChatCompletionRequestUserMessage, ChatCompletionRequestUserMessageArgs, ChatCompletionRequestUserMessageContent, CreateChatCompletionRequestArgs, ImageUrl};
+use async_openai::types::{ChatCompletionRequestMessage, ChatCompletionRequestMessageContentPart, ChatCompletionRequestMessageContentPartImageArgs, ChatCompletionRequestMessageContentPartTextArgs, ChatCompletionRequestUserMessageArgs, ChatCompletionRequestUserMessageContent, CreateChatCompletionRequestArgs, ImageUrl};
 use image::DynamicImage;
 use lazy_static::lazy_static;
-use macro_utils::SerializeError;
-use serde::de::Unexpected::Option;
-use thiserror::Error;
 
 lazy_static! {
     static ref OPENAI_CLIENT: async_openai::Client<OpenAIConfig> = async_openai::Client::new();
 }
 
 fn image_to_base64url(image: DynamicImage) -> String {
-    let image = image.to_rgba8();
-    let image = image.into_vec();
-    let image = base64::encode(image);
-    let image = format!("data:image/png;base64,{}", image);
-    image
+    let mut image_data = Cursor::new(Vec::new());
+    image.write_to(&mut image_data, image::ImageOutputFormat::Jpeg(75)).expect("Unable to write image to buffer");
+    let image_base64 = base64::encode(&image_data.get_ref());
+    let image_url = format!("data:image/jpeg;base64,{}", image_base64);
+    image_url
 }
 
 pub async fn process_image(image: DynamicImage) -> Result<String, async_openai::error::OpenAIError> {
